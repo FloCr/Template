3c46e3e4305ad8cc530f75246a1177c4f5e16421
Florian Carrière <56516660+FloCr@users.noreply.github.com>
2023-11-07 20:01:01 +0100

    Update wagon+devise-bootstrap.rb

--- wagon+devise-bootstrap.rb
+++ wagon+devise-bootstrap.rb
@@ -4,7 +4,6 @@ run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
 ########################################
 inject_into_file "Gemfile", before: "group :development, :test do" do
   <<~RUBY
-    # gem "bootstrap", "~> 5.2"
     gem "devise"
     gem "autoprefixer-rails"
     gem "font-awesome-sass", "~> 6.1"
